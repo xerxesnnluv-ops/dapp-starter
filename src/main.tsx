80eca2dcdf9327946402800aee9233df6e301748
xerxesnnluv-ops <xerxesnnluv@gmail.com>
2025-09-07 19:48:31 +0800

    fix: clean main.tsx React import

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
-// src/main.tsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
-import { wagmiConfig } from './web3'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const qc = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={qc}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>
+// React 18 新寫法
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
